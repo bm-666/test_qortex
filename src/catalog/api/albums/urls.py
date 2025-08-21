f99ac5ae53c9c230d9ebe045f636ee50cda0e096
bm-666 <artem.vostrikov.777@gmail.com>
2025-08-21 15:52:06 +0500

    Update urls.py
    fix endpoints

--- src/catalog/api/albums/urls.py
+++ src/catalog/api/albums/urls.py
@@ -7,7 +7,7 @@ from .views import (
 
 urlpatterns = [
     path('', albums_view, name='album-view'),
-    path('albums/<int:album_id>/', get_album, name='get_album'),
-    path('albums/<int:album_id>/songs/', add_song_to_album, name='add-song-to-album'),
+    path('<int:album_id>/', get_album, name='get_album'),
+    path('<int:album_id>/songs/', add_song_to_album, name='add-song-to-album'),
 
 ]
